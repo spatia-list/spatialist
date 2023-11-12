e4616894ec3bbe9ccc677fe2777b94b0a9915840
Deepana Ishtaweera <deepanaishtaweera174@gmail.com>
2023-11-12 15:53:15 +0100

    creating multiple post-its and change properties

--- Assets/Scripts/PostItCreator.cs
+++ Assets/Scripts/PostItCreator.cs
@@ -11,15 +11,24 @@ public class PostItCreator : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject obj;
+    private GameObject[] postIts = new GameObject[5];
+    private string[] postItColors = new string[] { "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#00FFFF" };
+    private Vector3[] postItPositions = new Vector3[] { new Vector3(0, 0, 1), new Vector3(0, 1, 1), new Vector3(0, -1, 1), new Vector3(1, 0, 1), new Vector3(-1, 0, 1) };
     private GameObject postIt;
     private float updateInterval = 0.5f; // Update every 0.5 seconds
     public float lastUpdateTime;
 
     void Start()
     {
-        this.postIt = Instantiate(obj, new Vector3(0, 0, 1), Quaternion.identity);
-        this.postIt.GetComponentInChildren<PostItUpdater>().UpdateText("The default text of post-it 4");
-        this.postIt.GetComponentInChildren<PostItUpdater>().UpdateColor("#FF0000");
+        for (int i = 0; i < 5; i++)
+        {
+            this.postIts[i] = Instantiate(obj, postItPositions[i], Quaternion.identity);
+            this.postIts[i].GetComponentInChildren<PostItUpdater>().UpdateText("The default text of post-it " + i);
+            this.postIts[i].GetComponentInChildren<PostItUpdater>().UpdateColor(postItColors[i]);
+        }
+        //this.postIt = Instantiate(obj, new Vector3(0, 0, 1), Quaternion.identity);
+        //this.postIt.GetComponentInChildren<PostItUpdater>().UpdateText("The default text of post-it 4");
+        //this.postIt.GetComponentInChildren<PostItUpdater>().UpdateColor("#FF0000");
 
     }
 
@@ -30,7 +39,11 @@ public class PostItCreator : MonoBehaviour
         {
             DateTime currenttime = DateTime.Now;
             string formattedTime = currenttime.ToString("hh:mm:ss");
-            this.postIt.GetComponentInChildren<PostItUpdater>().UpdateText("The post-it text at time: " + formattedTime);
+            for (int i = 0; i < 5; i++)
+            {
+                this.postIts[i].GetComponentInChildren<PostItUpdater>().UpdateText(i.ToString() + ": post-it text at time: " + formattedTime);
+            }
+            //this.postIt.GetComponentInChildren<PostItUpdater>().UpdateText("The post-it text at time: " + formattedTime);
             lastUpdateTime = Time.time;
         }
     }
--- Assets/Scripts/PostItUpdater.cs
+++ Assets/Scripts/PostItUpdater.cs
@@ -9,6 +9,7 @@ public class PostItUpdater : MonoBehaviour
 {
     private TextMeshProUGUI tmpText;
     //private TMP_Text tmpText;
+    private string postItId;
 
     void Start()
     {
@@ -20,6 +21,7 @@ public class PostItUpdater : MonoBehaviour
 
     }
 
+    // update text
     public void UpdateText(string text)
     {
         this.tmpText = gameObject.GetComponent<TextMeshProUGUI>();
@@ -27,10 +29,26 @@ public class PostItUpdater : MonoBehaviour
         Debug.Log($"text update signal -> {text}");
     }
 
+    // update color
     public void UpdateColor(string color)
     {
         this.tmpText = gameObject.GetComponent<TextMeshProUGUI>();
         this.tmpText.color = ColorUtility.TryParseHtmlString(color, out var c) ? c : Color.white;
         Debug.Log($"color update signal -> {color}");
+    }
+
+    // set id
+    public void UpdateId(string id)
+    {
+        this.postItId = id;
+        Debug.Log($"id update signal -> {id}");
+    }
+
+    // update background color
+    public void UpdateBackgroundColor(string color)
+    {
+        //this.tmpText = gameObject.GetComponent<TextMeshProUGUI>();
+        //this.tmpText.color = ColorUtility.TryParseHtmlString(color, out var c) ? c : Color.white;
+        Debug.Log($"background color update signal -> {color}");
     }
 }
