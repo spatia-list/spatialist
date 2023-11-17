bd5d8909f3caf14e30bef29349cc7c291365489e
Deepana Ishtaweera <deepanaishtaweera174@gmail.com>
2023-11-17 11:10:52 +0100

    fetch postits

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 using System.Text.Json;
 using Debug = UnityEngine.Debug;
 
+// public class PostIt
+// {
+//     public string id { get; set; }
+//     public string anchor_id { get; set; }
+//     public string owner { get; set; }
+//     public string title { get; set; }
+//     public string type { get; set; }
+//     public string text_content { get; set; }
+//     public string media_content { get; set; }
+//     public List<int> rgb { get; set; }
+//     public string _rid { get; set; }
+//     public string _self { get; set; }
+//     public string _etag { get; set; }
+//     public string _attachments { get; set; }
+//     public int _ts { get; set; }
+// }
+
+// public class PostItContainer
+// {
+//     public List<PostIt> postits { get; set; }
+// }
+
 public class NetworkManager : MonoBehaviour
 {
+    // endpoint url
+    private string url = "http://10.5.36.174:8000";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +49,7 @@ public class NetworkManager : MonoBehaviour
     {
         using (HttpClient client = new HttpClient())
         {
-            HttpResponseMessage response = await client.GetAsync("http://0.0.0.0:8000/postits");
+            HttpResponseMessage response = await client.GetAsync(this.url+"/postits");
             response.EnsureSuccessStatusCode();
             string responseBody = await response.Content.ReadAsStringAsync();
             return responseBody;
@@ -37,8 +62,10 @@ public class NetworkManager : MonoBehaviour
         yield return new WaitUntil(() => getPostItsTask.IsCompleted);
         string postItJson = getPostItsTask.Result;
 
-        // Rest of your code...
-
+        // print the json response
         Debug.Log(postItJson);
+
+        // deserialize the json response
+        // PostItContainer postItContainer = JsonSerializer.Deserialize<PostItContainer>(postItJson);
     }
 }
