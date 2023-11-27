a73a66299ddbee5a412686ab51d3a2aff98cf8ad
Malo Ranzetti <71760128+mrztti@users.noreply.github.com>
2023-11-27 14:23:07 +0100

    Add UI transitions (#24)
    * able to change text in textmeshprougui at update
    
    * change color and text dynamically in a prefab
    
    * creating multiple post-its and change properties
    
    * add debug print for the post-it creator script
    
    * work on api calls to backend to retrieve postits
    
    * UI elements are added
    
    * added UI scenes
    
    * Revert "added UI scenes"
    
    This reverts commit 26788855ba1d080b2d8ef8c7f614c255b9e211b1.
    
    * Revert "Revert "added UI scenes""
    
    This reverts commit fa5e43dfa3d960045c0817165850e8107a609879.
    
    * fetch postits
    
    * fetch and print post-it details
    
    * add post to post it class
    
    * start working on postitviz
    
    * UI prefabs
    
    * scene organization
    
    * work on refactoring
    
    * Add transition skeleton
    
    * Add Work on scene
    
    * button actions are defined
    
    ---------
    
    Co-authored-by: Deepana Ishtaweera <deepanaishtaweera174@gmail.com>
    Co-authored-by: Deepana Ishtaweera <dishtaweera@gmail.com>
    Co-authored-by: bcoban <bcoban@student.ethz.ch>
    Co-authored-by: Beyzanur Çoban <beyzanur.cobann@gmail.com>

---
+++ Assets/Scripts/UIStateBinder.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIStateBinder : MonoBehaviour
+{
+
+    public UIManager manager;
+
+    public void SetUIManagerState(UIState state)
+    {
+        if (manager == null) {
+            Debug.Log("Component tried to set state without attached UI manager!");
+            return;
+        }
+
+        manager.SetState(state);
+    }
+
+}
