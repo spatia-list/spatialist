ca56988230a073a8b0f335cab85422139b38bc2c
Zeno <zeno.hamers@gmail.com>
2023-12-08 02:36:05 +0100

    applying button template to slate (try, not working yet)

--- Assets/Scripts/ShowKeyboard.cs
+++ Assets/Scripts/ShowKeyboard.cs
@@ -2,26 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Microsoft.MixedReality.Toolkit.Experimental.UI;
+
 
 public class ShowKeyboard : MonoBehaviour
 {
+    public TouchScreenKeyboard keyboard;
     private TMP_InputField inputField;
 
     // Start is called before the first frame update
     void Start()
     {
         inputField = GetComponent<TMP_InputField>();
-        inputField.onSelect.AddListener(x => OpenKeyboard()); // will take a string as an imput and open the keyboard
-        
+        // inputField.OnSelect.AddListener(x => OpenSystemKeyboard()); // calls the function OpenSystemKeyboard(), the x => is necessary because the OnSelect event passes a string as an output parameter (the inputfield ID)
+    }
+
+    
+    public void OpenSystemKeyboard()
+    {  
+        string currentText = inputField.text; // Get the current text from the input field
+        keyboard = TouchScreenKeyboard.Open(currentText, TouchScreenKeyboardType.Default, false, false, false, false);
+        TouchScreenKeyboard.hideInput = true; // Hide the keyboard's input field
+
     }
 
     // Update is called once per frame
-    public void OpenKeyboard()
+    private void Update()
     {
-       NonNativeKeyboard.Instance.InputField = inputField;
-       NonNativeKeyboard.Instance.PresentKeyboard(inputField.text);
+        if (keyboard != null)
+        {
+            if (keyboard.status == TouchScreenKeyboard.Status.Done || keyboard.status == TouchScreenKeyboard.Status.Canceled)
+            {
+                // set the keyboard to null so it doesn't open again
+                keyboard = null;
+            }
+            else if (keyboard.active)
+            {
+                // show the typed text on the post-it (inputField)
+                inputField.text = keyboard.text;
+            }
 
-        
+        }
     }
+
 }
